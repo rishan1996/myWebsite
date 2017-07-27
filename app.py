57e1b98229088041fde279147aac9ff9c424a79b
rishan <rishan1996@hotmail.co.uk>
2017-07-27 21:48:18 +0100

    Updated python to route to all pages and added static images and css

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ def home():
 def portfolio():
     return 'You are on portfolio'
 
+@app.route('/contactus')
+def contactUs():
+    return 'You are on contact us'
+
+@app.route('/aboutus')
+def aboutUs():
+    return 'You are on about us'
+
 if __name__ == '__main__':
     app.run(debug=True)
